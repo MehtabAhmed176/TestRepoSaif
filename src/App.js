40624c7b99bbd09ef28744386d1056bebf1416bc
mehtab ahmed <mehtab.ahmed@webvisum.de>
2021-11-11 14:17:00 +0100

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import BoardCard from './components/BoardCard';
+import data from './components/data/cardData'
+import { useState } from 'react';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App() {
+  console.log('data', data)
+  const [tasks, setTasks] = useState([]);
+  
+  
+  const addTaskHandler = (task)=> {
+    console.log('task values', task)
+   // let newTaskArray = tasks.push(task)
+    setTasks(prevItems => [...prevItems, task]);
+    console.log('tasks from State', tasks)
+  }
+  
+  const Card = data.map((item,index) => (
+
+    <BoardCard title={item.title} tasks={tasks} addTaskHandler={addTaskHandler} index={index}/>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    
+    <DndProvider backend={HTML5Backend}>
+     <div style={{ display: "flex", pading: "100px", width: "100%", margin: "auto", flexWrap: "wrap", backgroundColor: "blue" }}>
+        {Card}
+      </div>
+      </DndProvider>
+   
+  )
 }
 
 export default App;
